ac998f0287ad4facf4c7077b1e8e5a98cda893a9
Emanuel <emanuelszevaga7@gmail.com>
2025-10-14 20:36:54 -0300

    primeras actividades

---
+++ act3.js
@@ -0,0 +1,12 @@
+let x = $(document);
+x.ready(inicializarEventos);
+
+function inicializarEventos() {
+let x = $("li");
+x.click(presionItem);
+}
+
+function presionItem() {
+let x = $(this);
+x.hide();
+}
